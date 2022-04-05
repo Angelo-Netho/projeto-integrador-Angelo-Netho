98639c98cb26ec655b4c1b06861a68db752c9827
Angelo Netho <angelonetho@gmail.com>
2022-04-05 20:38:01 -0300

    Create new javafx references in Login.java

--- jupiter/src/main/java/dev/netho/jupiter/controller/Login.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/Login.java
@@ -1,5 +1,26 @@
 package dev.netho.jupiter.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
 public class Login {
 
+    @FXML
+    public GridPane root;
+
+    @FXML
+    public TextField tfEmail;
+
+    @FXML
+    public PasswordField tfPassword;
+
+    @FXML
+    public CheckBox cbRemember;
+
+    @FXML
+    public Button btConfirm;
+
+    @FXML
+    public Hyperlink hlHelp;
 }
