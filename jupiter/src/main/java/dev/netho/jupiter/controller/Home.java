5a869a446e182d652461824f0f825f8c299ac0ad
Angelo Netho <angelonetho@gmail.com>
2022-04-05 21:50:32 -0300

    Fix fxml unknown load bug

--- jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
@@ -3,25 +3,19 @@ package dev.netho.jupiter.controller;
 import dev.netho.jupiter.Main;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
-import javafx.util.Callback;
 
 public class Home {
 
     @FXML
     private StackPane centralPanel;
 
-
     public void initialize() {
         loadLogin();
     }
 
     private void loadLogin() {
-        loadView(centralPanel, "/dev/netho/fxml/login.fxml", aClass -> new Login());
-
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadTela("/dev/netho/fxml/login.fxml", (o) -> new Login()));
     }
 
-    private void loadView(StackPane stackPane, String reference,  Callback controller) {
-        stackPane.getChildren().clear();
-        stackPane.getChildren().add(Main.loadTela(reference, (o) -> controller));
-    }
 }
