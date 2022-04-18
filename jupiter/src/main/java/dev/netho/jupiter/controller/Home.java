f710f8d15a95f6d5569c7ec72b1cdb4657ff861d
Angelo Netho <angelonetho@gmail.com>
2022-04-18 13:10:47 -0300

    Implements dual screen redirect

--- jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
@@ -1,6 +1,8 @@
 package dev.netho.jupiter.controller;
 
 import dev.netho.jupiter.Main;
+import dev.netho.jupiter.models.Psychologist;
+import dev.netho.jupiter.repositories.PatientRepository;
 import dev.netho.jupiter.services.AuthService;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
@@ -12,8 +14,11 @@ public class Home {
 
     private final AuthService authService;
 
-    public Home(AuthService authService) {
+    private final PatientRepository patientRepository;
+
+    public Home(AuthService authService, PatientRepository patientRepository) {
         this.authService = authService;
+        this.patientRepository = patientRepository;
     }
 
     public void initialize() {
@@ -24,7 +29,11 @@ public class Home {
         if(!authService.loggedIn()) {
             loadScreen(Screen.LOGIN);
         }else {
-            loadScreen(Screen.DASHBOARD);
+            if(authService.getProfile() instanceof Psychologist) {
+                loadScreen(Screen.PSYCHOLOGIST_HOME);
+            } else {
+                loadScreen(Screen.PATIENT_HOME);
+            }
         }
     }
 
@@ -35,9 +44,14 @@ public class Home {
             centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new Login(authService, this)));
         }
 
-        if (screen == Screen.DASHBOARD) {
+        if (screen == Screen.PSYCHOLOGIST_HOME) {
             centralPanel.getChildren().clear();
-            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new HomeAdmin()));
+            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new HomePsychologist(authService)));
+        }
+
+        if (screen == Screen.PATIENT_HOME) {
+            centralPanel.getChildren().clear();
+            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new HomePatient(authService, this, patientRepository)));
         }
     }
 
