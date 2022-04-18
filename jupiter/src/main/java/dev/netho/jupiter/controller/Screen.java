65f381395f264917c34c47168c4ca1e4663c9a93
Angelo Netho <angelonetho@gmail.com>
2022-04-18 12:50:45 -0300

    Add new screens enums

--- jupiter/src/main/java/dev/netho/jupiter/controller/Screen.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/Screen.java
@@ -3,7 +3,9 @@ package dev.netho.jupiter.controller;
 public enum Screen {
 
     LOGIN("/dev/netho/fxml/login.fxml"),
-    DASHBOARD("/dev/netho/fxml/home_admin.fxml");
+    PSYCHOLOGIST_HOME("/dev/netho/fxml/home_psychologist.fxml"),
+    PATIENT_HOME("/dev/netho/fxml/home_patient.fxml"),
+    PATIENT_PROFILE("/dev/netho/fxml/profile_patient.fxml");
 
     private final String source;
 
