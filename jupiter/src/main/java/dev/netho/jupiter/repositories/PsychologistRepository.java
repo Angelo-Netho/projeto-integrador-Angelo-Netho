287bd7c19fb7a560ed561b21002c8e384ab3c3c6
Angelo Netho <angelonetho@gmail.com>
2022-04-18 21:02:14 -0300

    Implements patch for Psychologist

--- jupiter/src/main/java/dev/netho/jupiter/repositories/PsychologistRepository.java
+++ jupiter/src/main/java/dev/netho/jupiter/repositories/PsychologistRepository.java
@@ -7,6 +7,7 @@ import dev.netho.jupiter.models.Patient;
 import dev.netho.jupiter.models.Psychologist;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class PsychologistRepository {
@@ -21,16 +22,26 @@ public class PsychologistRepository {
         this.diaryDAO = diaryDAO;
     }
 
-    public Psychologist getPsychologist(int id) throws Exception {
+    public Psychologist getPsychologist(int id) throws SQLException {
         Psychologist psychologist = psychologistDAO.getPsychologist(id);
 
         return buildPsychologist(psychologist);
     }
 
-    public Psychologist getPsychologist(String email) throws Exception {
+    public Psychologist getPsychologist(String email) throws SQLException {
         Psychologist psychologist = psychologistDAO.getPsychologist(email);
 
         return buildPsychologist(psychologist);
+    }
+
+    public boolean patchPsychologist(int id, String name, String email, String phone, String gender, LocalDate birthday, LocalDate ingress, String profilePicture, String crp) throws SQLException {
+        Psychologist psychologist = new Psychologist(id, name, email, phone, gender, birthday, ingress, profilePicture, crp, null);
+
+        return psychologistDAO.patchPsychologist(id, psychologist);
+    }
+
+    public boolean deletePsychologist(int id) throws SQLException {
+        return psychologistDAO.deletePsychologist(id);
     }
 
     private Psychologist buildPsychologist(Psychologist psychologist) throws SQLException {
