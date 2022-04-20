b6d3d39ca814841cb42539c50b360362d794d164
Angelo Netho <angelonetho@gmail.com>
2022-04-20 00:56:25 -0300

    Create new load methods for new views

--- jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/Home.java
@@ -2,12 +2,18 @@ package dev.netho.jupiter.controller;
 
 import dev.netho.jupiter.Main;
 import dev.netho.jupiter.models.Psychologist;
+import dev.netho.jupiter.repositories.DiaryRepository;
 import dev.netho.jupiter.repositories.PatientRepository;
+import dev.netho.jupiter.repositories.PsychologistRepository;
 import dev.netho.jupiter.services.AuthService;
 import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 public class Home {
+
+    @FXML
+    public BorderPane root;
 
     @FXML
     private StackPane centralPanel;
@@ -15,10 +21,14 @@ public class Home {
     private final AuthService authService;
 
     private final PatientRepository patientRepository;
+    private final PsychologistRepository psychologistRepository;
+    private final DiaryRepository diaryRepository;
 
-    public Home(AuthService authService, PatientRepository patientRepository) {
+    public Home(AuthService authService, PatientRepository patientRepository, PsychologistRepository psychologistRepository, DiaryRepository diaryRepository) {
         this.authService = authService;
         this.patientRepository = patientRepository;
+        this.psychologistRepository = psychologistRepository;
+        this.diaryRepository = diaryRepository;
     }
 
     public void initialize() {
@@ -41,17 +51,17 @@ public class Home {
 
         if (screen == Screen.LOGIN) {
             centralPanel.getChildren().clear();
-            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new Login(authService, this)));
+            centralPanel.getChildren().add(Main.loadScreen(screen.getSource(), (o) -> new Login(authService, this)));
         }
 
         if (screen == Screen.PSYCHOLOGIST_HOME) {
             centralPanel.getChildren().clear();
-            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new HomePsychologist(authService)));
+            centralPanel.getChildren().add(Main.loadScreen(screen.getSource(), (o) -> new HomePsychologist(authService, this, psychologistRepository, patientRepository)));
         }
 
         if (screen == Screen.PATIENT_HOME) {
             centralPanel.getChildren().clear();
-            centralPanel.getChildren().add(Main.loadTela(screen.getSource(), (o) -> new HomePatient(authService, this, patientRepository)));
+            centralPanel.getChildren().add(Main.loadScreen(screen.getSource(), (o) -> new HomePatient(authService, this, patientRepository, diaryRepository)));
         }
     }
 
--- jupiter/src/main/java/dev/netho/jupiter/controller/HomePatient.java
+++ jupiter/src/main/java/dev/netho/jupiter/controller/HomePatient.java
@@ -1,7 +1,9 @@
 package dev.netho.jupiter.controller;
 
 import dev.netho.jupiter.Main;
+import dev.netho.jupiter.models.Diary;
 import dev.netho.jupiter.models.Patient;
+import dev.netho.jupiter.repositories.DiaryRepository;
 import dev.netho.jupiter.repositories.PatientRepository;
 import dev.netho.jupiter.services.AuthService;
 import javafx.fxml.FXML;
@@ -9,10 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
 public class HomePatient {
-
-    private final AuthService authService;
-    private final Home home;
-    private final Patient patient;
 
     @FXML
     public StackPane centralPanel;
@@ -29,35 +27,71 @@ public class HomePatient {
     @FXML
     public Button btnPreferences;
 
+    private final AuthService authService;
+    private final Home home;
+    private final Patient patient;
     private final PatientRepository patientRepository;
+    private final DiaryRepository diaryRepository;
 
-
-    public HomePatient(AuthService authService, Home home, PatientRepository patientRepository) {
+    public HomePatient(AuthService authService, Home home, PatientRepository patientRepository, DiaryRepository diaryRepository) {
         this.authService = authService;
         this.home = home;
         this.patientRepository = patientRepository;
+        this.diaryRepository = diaryRepository;
         patient = (Patient) authService.getProfile();
     }
 
     public void initialize() {
-        updateButtonRender(btnMainPage);
+        loadDashboard();
 
     }
 
+    @FXML
+    public void loadDashboard() {
+        updateButtonRender(btnMainPage);
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadScreen(Screen.PATIENT_DASHBOARD.getSource(), (o) -> new DashboardPatient(patient, this)));
+    }
+
+    @FXML
+    public void loadDiaries() {
+        updateButtonRender(btnDiary);
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadScreen(Screen.LIST_DIARIES.getSource(), (o) -> new ListDiaries(patient, this, patientRepository)));
+    }
+
+    @FXML
     public void loadProfile() {
         updateButtonRender(btnProfile);
         centralPanel.getChildren().clear();
-        centralPanel.getChildren().add(Main.loadTela(Screen.PATIENT_PROFILE.getSource(), (o) -> new ProfilePatient(patient, patientRepository)));
+        centralPanel.getChildren().add(Main.loadScreen(Screen.PATIENT_PROFILE.getSource(), (o) -> new ProfilePatient(patient, patientRepository)));
+    }
+    @FXML
+    public void loadPreferences() {
+        updateButtonRender(btnPreferences);
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadScreen(Screen.PREFERENCES.getSource(), (o) -> new Preferences(home)));
     }
 
-
+    @FXML
     public void logOut() {
         authService.logOut();
         home.update();
-
     }
 
-    public void updateButtonRender(Button selected) {
+    public void loadDiaryEditor(Patient patient, Diary diary) {
+        updateButtonRender(btnDiary);
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadScreen(Screen.DIARY_EDITOR.getSource(), (o) -> new DiaryEditor(patient, diary, diaryRepository, this)));
+    }
+
+    public void loadDiaryEditor(Patient patient) {
+        updateButtonRender(btnDiary);
+        centralPanel.getChildren().clear();
+        centralPanel.getChildren().add(Main.loadScreen(Screen.DIARY_EDITOR.getSource(), (o) -> new DiaryEditor(patient, diaryRepository, this)));
+    }
+
+    private void updateButtonRender(Button selected) {
         String notSelectedStyle = "-fx-background-color: rgba(0, 247, 99, 0);-fx-opacity: 0.5;";
 
         btnMainPage.setStyle(notSelectedStyle);
